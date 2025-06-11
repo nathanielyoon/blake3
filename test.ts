ce19cfb77710a13c5f0ff1ef56816e11063c2a9c
nathanielyoon <nathanielyoon1@gmail.com>
2025-06-10 23:45:39 -0400

    fix: update versions

--- test.ts
+++ test.ts
@@ -1,12 +1,13 @@
-import { s16_b } from "jsr:@nyoon/base@^1.0.7/16";
+import { s16_b } from "jsr:@nyoon/base@^2.0.0";
 import { assertEquals } from "jsr:@std/assert@^1.0.13";
 import { blake3_derive, blake3_hash, blake3_keyed } from "./mod.ts";
 
 Deno.test("reference vectors", () =>
   fetch(
     "https://raw.githubusercontent.com/BLAKE3-team/BLAKE3/ae3e8e6b3a5ae3190ca5d62820789b17886a0038/test_vectors/test_vectors.json",
-  ).then(($) => $.text()).then(($) => {
-    const { key, context_string, cases } = JSON.parse($);
+  ).then<
+    { key: string; context_string: string; cases: Record<string, string>[] }
+  >(($) => $.json()).then(({ key, context_string, cases }) => {
     const a = new TextEncoder().encode(key);
     const b = new TextEncoder().encode(context_string);
     cases.forEach(($: Record<string, string>) => {
